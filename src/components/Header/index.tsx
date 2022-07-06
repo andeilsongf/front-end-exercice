a0f98678fe9ddbd7c3ce4f6fc2dc95e11e3b4b30
Andeilson Ferreira <agf@Mac-Pro-de-Andeilson.local>
2022-07-06 17:12:13 -0300

    styled component header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,9 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
+    <div className="w-full absolute top-0 bg-[#8257e5]">
       <nav>
-        <ul>
+        <ul className="flex h-16 items-center justify-center gap-5 text-white">
           <Link to="/">Generator</Link>
           <Link to="/payment">Payment</Link>
         </ul>
